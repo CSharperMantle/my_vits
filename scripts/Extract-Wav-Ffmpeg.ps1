ba9c8ba6f1702e50592d005a9181868b40f62b0d
Rong "Mantle" Bao <baorong2005@126.com>
2023-06-14 14:06:31 +0800

    Update files

--- scripts/Extract-Wav-Ffmpeg.ps1
+++ scripts/Extract-Wav-Ffmpeg.ps1
@@ -1,9 +1,53 @@
+<#
+.SYNOPSIS
+    Extract audio tracks in WAV from video files with FFMPEG.
+
+.DESCRIPTION
+    This script iterates over all files in given path and feeds them
+    to FFMPEG. Extracted WAV contents are placed next to original
+    video with the same base name. No format check is performed
+    during this process.
+
+.NOTES
+    `ffmpeg.exe` should be in `$env:PATH` for this script to run
+    normally.
+
+    This script is licensed under a MIT license.
+    Copyright (c) 2023 Rong Bao <baorong2005@126.com>
+.PARAMETER Path
+    Working path to find video files in.
+.PARAMETER WhatIf
+    Show operations without actually running.
+#>
+
+<#
+ # Copyright (c) 2023 Rong Bao <baorong2005@126.com>
+ # 
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documentation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to whom the Software is
+ # furnished to do so, subject to the following conditions:
+ # 
+ # The above copyright notice and this permission notice shall be included in all
+ # copies or substantial portions of the Software.
+ # 
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ # SOFTWARE.
+ #>
+
 param (
     [Parameter(Mandatory = $true,
         Position = 0,
         ValueFromPipeline = $true,
         ValueFromPipelineByPropertyName = $true,
-        HelpMessage = "Working path")]
+        HelpMessage = "Working path 1")]
     [Alias("PSPath")]
     [ValidateNotNullOrEmpty()]
     [string]
--- scripts/Make-Filelist.ps1
+++ scripts/Make-Filelist.ps1
@@ -1,3 +1,53 @@
+<#
+.SYNOPSIS
+    Match WAV audio files with their TXT transcriptions.
+
+.DESCRIPTION
+    This script iterates over all WAV files in given path, finding its
+    TXT transcription file in the same path, whose name contains the base
+    name of the WAV file as its prefix. It then produces a TXT filelist
+    containing matched pairs with path to audio and content of transcription,
+    separated by given delimiter.
+
+.NOTES
+    WAV files having multiple transcriptions will be skipped. Having only one
+    contentless transcription will also cause the WAV file to be skipped.
+
+    This script is licensed under a MIT license.
+    Copyright (c) 2023 Rong Bao <baorong2005@126.com>
+
+.PARAMETER Path
+    Working path to find video files in.
+.PARAMETER FilelistName
+    Name of filelist to create.
+.PARAMETER Delim
+    Character used as delimiter.
+.PARAMETER PunctChar
+    Character substituting line breaks and spaces.
+#>
+
+<#
+ # Copyright (c) 2023 Rong Bao <baorong2005@126.com>
+ # 
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documentation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to whom the Software is
+ # furnished to do so, subject to the following conditions:
+ # 
+ # The above copyright notice and this permission notice shall be included in all
+ # copies or substantial portions of the Software.
+ # 
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ # SOFTWARE.
+ #>
+
 param (
     [Parameter(Mandatory = $true,
         Position = 0,
@@ -19,6 +69,14 @@ param (
 
     [Parameter(Mandatory = $false,
         Position = 2,
+        HelpMessage = "Delimiter")]
+    [Alias("D")]
+    [ValidateNotNullOrEmpty()]
+    [char]
+    $Delim = "|",
+
+    [Parameter(Mandatory = $false,
+        Position = 3,
         HelpMessage = "Character for indicating punctuation")]
     [Alias("PC")]
     [ValidateNotNullOrEmpty()]
@@ -50,7 +108,7 @@ foreach ($f in $wav_files) {
         continue
     }
 
-    $filelist_items.Add("$($f.FullName)|$transcription")
+    $filelist_items.Add("$($f.FullName)$Delim$transcription")
 }
 
 $final_content = $filelist_items -join [System.Environment]::NewLine
